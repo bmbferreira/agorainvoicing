745f3c13517a782875ee63339e193482dddb3117
Bruno Ferreira <bmibferreira@gmail.com>
2018-12-20 14:20:58 +0000

    Update ApiKey.php

--- app/ApiKey.php
+++ app/ApiKey.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ApiKey extends Model
 {
+    protected $table2 = 'api_keys2';
     protected $table = 'api_keys';
     protected $fillable = ['rzp_key', 'rzp_secret', 'apilayer_key', 'bugsnag_api_key',
     'zoho_api_key', 'msg91_auth_key', 'twitter_consumer_key',
